d3070edd71bf610f4351cffe58450e8ae93950e7
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-06 10:25:49 -0800

    adding files

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+import { createImg } from "./emblemGenerator.js"
+
+const renderLoc = document.getElementById("characterEmblemRender")
+const emblemText = document.getElementById("emblemTextForm")
+const submitButton = document.getElementById("generateBtn")
+const oldEmblems = document.getElementById("oldEmblems")
+
+oldEmblems
+
+
+submitButton.onclick = function() {
+    moveEmblem()
+    // clear render area
+    renderLoc.innerHTML = '';
+    
+    // generate sub-area
+    renderLoc.innerHTML = `
+        <div class="generatedEmblem">
+
+        <p>${emblemText.value}</p>
+        </div>
+    `
+
+    // get id of new render area
+    const emblemLoc = renderLoc.getElementsByClassName("generatedEmblem")
+    // generate
+    createImg(emblemLoc[0], emblemText.value);
+ }
+
+function moveEmblem() {
+    if (renderLoc.innerHTML != '') {
+        oldEmblems.innerHTML += renderLoc.innerHTML
+    }
+}
